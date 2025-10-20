d7c7ae83795729f6508e3a4c4f05de5d9b207179
kirillrjev <kirillrjev@gmail.com>
2025-10-20 15:36:25 +0300

    Add files via upload

---
+++ Pr04/Pr04/Pr04/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            
+            Console.WriteLine("Введите количество элементов массива:");
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            Console.WriteLine("Введите элементы массива:");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Элемент {i + 1}: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("\nИсходный массив:");
+            PrintArray(array);
+            SelectionSort(array);
+            Console.WriteLine("\nОтсортированный массив (по возрастанию):");
+            PrintArray(array);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Ошибка: введено некорректное число.");
+        }
+        catch (OutOfMemoryException)
+        {
+            Console.WriteLine("Ошибка: недостаточно памяти для создания массива.");
+        }
+        catch (IndexOutOfRangeException)
+        {
+            Console.WriteLine("Ошибка: выход за границы массива.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Неизвестная ошибка: {ex.Message}");
+        }
+    }
+    static void SelectionSort(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++)
+            {
+                if (arr[j] < arr[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (int num in arr)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Pr04/Pr04/Pr04_17 Вариант/Pr04_17/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Введите количество элементов массива: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] array = new int[n];
+            Console.WriteLine("Введите элементы массива:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Элемент {i + 1}: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nИсходный массив:");
+            PrintArray(array);
+            int positiveCount = 0;
+            foreach (int num in array)
+            {
+                if (num > 0)
+                    positiveCount++;
+            }
+            Console.WriteLine($"\nКоличество положительных элементов: {positiveCount}");
+            int lastZeroIndex = -1;
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                if (array[i] == 0)
+                {
+                    lastZeroIndex = i;
+                    break;
+                }
+            }
+
+            int sumAfterZero = 0;
+            if (lastZeroIndex != -1 && lastZeroIndex < array.Length - 1)
+            {
+                for (int i = lastZeroIndex + 1; i < array.Length; i++)
+                {
+                    sumAfterZero += array[i];
+                }
+            }
+            Console.WriteLine($"Сумма элементов после последнего нуля: {sumAfterZero}");
+            List<int> lessOrEqualOne = new List<int>();
+            List<int> greaterThanOne = new List<int>();
+
+            foreach (int num in array)
+            {
+                if (num <= 1)
+                    lessOrEqualOne.Add(num);
+                else
+                    greaterThanOne.Add(num);
+            }
+            List<int> transformed = new List<int>();
+            transformed.AddRange(lessOrEqualOne);
+            transformed.AddRange(greaterThanOne);
+
+            Console.WriteLine("\nПреобразованный массив:");
+            PrintArray(transformed.ToArray());
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Ошибка: введено нецелое число.");
+        }
+        catch (OutOfMemoryException)
+        {
+            Console.WriteLine("Ошибка: недостаточно памяти.");
+        }
+        catch (IndexOutOfRangeException)
+        {
+            Console.WriteLine("Ошибка: выход за границы массива.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Неизвестная ошибка: {ex.Message}");
+        }
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (int num in arr)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+}
